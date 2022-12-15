b9b314dc526ddcf0b8e485b8ba5a7f932dcfc4e4
Leonardo Santos <leonardoaugusto.dev@gmail.com>
2022-12-15 11:32:53 -0300

    Fix lower case in the bovespa integration

--- plugins/brapi.js
+++ plugins/brapi.js
@@ -2,8 +2,9 @@ export default ({ app, store }, inject) => {
     inject('brapi', {
         async getQuotes(symbols, range = '1d', interval = '1d', fundamental = true) {
             try{
+                const lowerCaseSymbols = Array.isArray(symbols) ? symbols.map(s => s.toLowerCase()) : symbols.toLowerCase()
                 const response = await app.$axios.$get(
-                    `https://brapi.dev/api/quote/${symbols}?range=${range}&interval=${interval}&fundamental=${fundamental}`
+                    `https://brapi.dev/api/quote/${lowerCaseSymbols}?range=${range}&interval=${interval}&fundamental=${fundamental}`
                 );
                 return response;
             }
